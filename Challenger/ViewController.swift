a0a750335eb71c5526edacf817b3b0e7642a65d2
Jack Song <133342301@163.com>
2015-12-01 15:28:05 +0800

    Initial Commit

---
+++ Challenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Challenger
+//
+//  Created by SongXujie on 1/12/2015.
+//  Copyright © 2015 SK8 PTY LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
