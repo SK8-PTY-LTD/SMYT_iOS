ff1d79c848313484686636833a969c94ca1aed4e
Jack Song <133342301@163.com>
2015-12-07 20:32:46 +0800

    Initialised project
    30% Done with camera
    Dragged out the tab layouts

--- Challenger/AppDelegate.swift
+++ Challenger/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Initialize AVOSCloud
+        AVOSCloud.setApplicationId("UjFi1SbKQAXpNaGqOjtGalDR", clientKey: "G0OGS5h97g7IdvoID70izkFX");
+        AVAnalytics.trackAppOpenedWithLaunchOptions(launchOptions);
+        
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-Challenger/Pods-Challenger-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ChallengerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ChallengerVersionString[];
+
